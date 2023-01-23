fa9fee6c30e4c24a4b6aebb9f3de361676ad5647
Yan Marinho <yankaiquecosta.yk@gmail.com>
2023-01-23 08:22:32 -0300

    ✨ creating summary component base

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
-import { Transactions } from "./pages/Transactions/indes";
+import { Transactions } from "./pages/Transactions";
 
 export function App() {
   return (
